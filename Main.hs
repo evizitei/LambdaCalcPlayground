d899f1051198f239e408fb0b5990ba0af08a1ffa
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-03-27 09:23:37 -0500

    first term printed

--- Main.hs
+++ Main.hs
@@ -1,27 +1,9 @@
-import Src.Terms as Terms
+import Src.LambdaCalc
 
--- use the max version of current name stream, get one value higher
-newHint :: Name -> Name
-newHint (Nm chr idx) = Nm chr (idx + 1)
 
-actualFreshName :: [Name] -> Name -> Name
-actualFreshName ctx hint = 
-    case ctx of
-        [] -> hint
-        (head:otherNames) -> 
-            let collision = nameCollision(head, hint) in
-            if collision then
-                actualFreshName names (newHint head)
-            else
-                actualFreshName names hint
-
-printTerm :: Term -> [Name] -> String
-printTerm (TmAbs info name bodyTerm) ctx = 
-    let (ctx', name') = pickFreshName ctx name in
-    "(λ" ++ name' ++ ". " ++ printTerm(bodyTerm ctx') ++ ")"
-printTerm (TmVar info index depth) ctx = "TmVar " ++ show info ++ " " ++ show index ++ " " ++ show depth
 
 main :: IO ()
 main = do
-    let term = TmVar (Index 0) (Depth 0)
-    print term
+    let term = TmVar (Index 0) (Depth 1)
+    let ctx = [Nm "a" 0]
+    print(printTerm term ctx)
---
+++ src/LambdaCalc.hs
@@ -0,0 +1,66 @@
+-- Chapter 
+module Src.LambdaCalc where
+
+newtype Index = Index Int deriving (Show)
+newtype Depth = Depth Int deriving (Show)
+
+-- like a0, a1, a2, etc
+data Name = Nm String Int deriving (Show, Eq, Ord)
+
+relatedNames :: Name -> Name -> Bool
+relatedNames (Nm chra _) (Nm chrb _) = chra == chrb
+
+nameCollision :: Name -> Name -> Bool
+nameCollision exst@(Nm chr_a idx_a) hint@(Nm chr_b idx_b) = chr_a == chr_b && idx_a >= idx_b
+
+data Term = TmVar Index Depth
+    | TmAbs Name Term
+    | TmApp Term Term
+    deriving (Show)
+
+
+-- use the max version of current name stream, get one value higher
+newHint :: Name -> Name
+newHint (Nm chr idx) = Nm chr (idx + 1)
+
+actualFreshName :: [Name] -> Name -> Name
+actualFreshName ctx hint = 
+    case ctx of
+        [] -> hint
+        (head:otherNames) -> 
+            let collision = nameCollision head hint in
+            if collision then
+                actualFreshName otherNames (newHint head)
+            else
+                actualFreshName otherNames hint
+
+pickFreshName :: [Name] -> Name -> ([Name], Name)
+pickFreshName ctx hint = 
+    let freshName = actualFreshName ctx hint in
+    (freshName:ctx, freshName)
+
+indexToName :: Index -> [Name] -> String
+indexToName (Index i) ctx = 
+    let ctxLen = length ctx in
+    if i < ctxLen then
+        let Nm chr idx = (ctx !! i) in
+        chr ++ show idx
+    else
+        "[BAD I2N INDEX]"
+
+printTerm :: Term -> [Name] -> String
+printTerm (TmAbs name bodyTerm) ctx = 
+    let (ctx', name') = pickFreshName ctx name in
+    let bodyStr = printTerm bodyTerm ctx' in
+    let Nm n_chr n_idx = name' in
+    "(λ" ++ (n_chr ++ show n_idx) ++ ". " ++ bodyStr ++ ")"
+printTerm (TmApp t1 t2) ctx =
+    let t1Str = printTerm t1 ctx in
+    let t2Str = printTerm t2 ctx in
+    "(" ++ t1Str ++ " " ++ t2Str ++ ")"
+printTerm (TmVar index (Depth dInt)) ctx = 
+    let ctxLen = length ctx in
+    if ctxLen == dInt then
+        indexToName index ctx
+    else
+        "[BAD DEPTH INDEX]"
