50be8d2cc54f9f64b5babb687d4fef2f571b44c9
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-03-27 09:30:57 -0500

    show ability to print labmda and context for application construct

--- Main.hs
+++ Main.hs
@@ -10,3 +10,7 @@ main = do
     let ldb = TmAbs (Nm "x" 0)(TmVar (Index 0) (Depth 1))
     let ctx2 = []
     print(printTerm ldb ctx2)
+    let newLbd = TmAbs (Nm "x" 0)(TmVar (Index 0) (Depth 2))
+    let applic = TmApp newLbd (TmVar (Index 0) (Depth 1))
+    let ctx3 = [Nm "a" 0]
+    print(printTerm applic ctx3)
